9ce1c46c2402bb34ff7acb4f48d8b7a01d407681
Konstantin Bondarenko <kosntantinbo92@gmail.com>
2020-05-02 21:58:52 +0300

    Updates

---
+++ precache-manifest.af7184a0c2fa8cf32e03ef348c1570e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d86cef3c6cebdc566de11ed9c7674204",
+    "url": "/github.io/chucks-jokes/index.html"
+  },
+  {
+    "revision": "fe86bb77c5138b842936",
+    "url": "/github.io/chucks-jokes/static/css/main.692fedee.chunk.css"
+  },
+  {
+    "revision": "0881563e9620f0626242",
+    "url": "/github.io/chucks-jokes/static/js/2.2b5f5cc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/chucks-jokes/static/js/2.2b5f5cc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe86bb77c5138b842936",
+    "url": "/github.io/chucks-jokes/static/js/main.6ab7d06a.chunk.js"
+  },
+  {
+    "revision": "61d422e3a204a1a49020",
+    "url": "/github.io/chucks-jokes/static/js/runtime-main.009cf885.js"
+  }
+]);
